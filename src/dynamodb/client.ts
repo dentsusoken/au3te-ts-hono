fba9e9bb57bb8fc8b0b296785d107ad1e71aa8c0
rio-tsukano <xtsukano.rio@contract.dentsusoken.com>
2025-03-03 18:13:27 +0900

    chore: Confirmation of local operation

--- src/dynamodb/client.ts
+++ src/dynamodb/client.ts
@@ -4,7 +4,7 @@ import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 export const createDynamoDBClient = (config?: DynamoDBClientConfig) => {
   const client = new DynamoDBClient({
     endpoint: process.env.DYNAMODB_ENDPOINT,
-    region: process.env.AWS_REGION,
+    region: process.env.AWS_DEFAULT_REGION,
     ...config,
   });
   return DynamoDBDocumentClient.from(client);
